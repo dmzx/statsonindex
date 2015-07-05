094de443411f3961df094a8aed4092570d1fa5a3
dmzx <info@dmzx-web.net>
2015-07-05 22:44:03 +0200

    Code update

--- event/listener.php
+++ event/listener.php
@@ -22,6 +22,8 @@ class listener implements EventSubscriberInterface
 
 	/** @var \phpbb\template\template */
 	protected $template;
+	
+	protected $user;
 
 	public function __construct(\phpbb\config\config $config, \phpbb\template\template $template, \phpbb\user $user)
 	{
@@ -32,7 +34,6 @@ class listener implements EventSubscriberInterface
 
 	static public function getSubscribedEvents()
 	{
-
 		return array(
 			'core.user_setup' => 'load_language_on_setup',
 			'core.index_modify_page_title'	=> 'main',
@@ -41,14 +42,12 @@ class listener implements EventSubscriberInterface
 
 	public function main($event)
 	{
-
 		$total_posts = $this->config['num_posts'];
 		$total_topics = $this->config['num_topics'];
 		$total_users = $this->config['num_users'];
 		$total_files = $this->config['num_files'];
 		$start_date		= date("d M Y, H:i:s", $this->config['board_startdate'] );
 		$boarddays 		= ceil((time() - $this->config['board_startdate']) / 86400);
-
 		$l_total_file_s = ($total_files == 0) ? 'TOTAL_FILES_ZERO' : 'TOTAL_FILES_OTHER';
 		$posts_per_day = $total_posts / $boarddays;
 		$l_posts_per_day_s = ($posts_per_day == 0) ? 'POSTS_PER_DAY_ZERO' : 'POSTS_PER_DAY_OTHER';
@@ -75,21 +74,21 @@ class listener implements EventSubscriberInterface
 		$users_per_year = $users_per_day * 364.25;
 		$l_users_per_year_s = ($users_per_year == 0) ? 'USERS_PER_YEAR_ZERO' : 'USERS_PER_YEAR_OTHER';
 
-	$this->template->assign_vars(array(
-	'POSTS_PER_DAY'		=> sprintf($this->user->lang[$l_posts_per_day_s], $posts_per_day),
-	'POSTS_PER_YEAR'	=> sprintf($this->user->lang[$l_posts_per_year_s], $posts_per_year),
-	'POSTS_PER_TOPIC'	=> sprintf($this->user->lang[$l_posts_per_topic_s], $posts_per_topic),
-	'POSTS_PER_USER'		=> sprintf($this->user->lang[$l_posts_per_user_s], $posts_per_user),
-	'TOPICS_PER_DAY'		=> sprintf($this->user->lang[$l_topics_per_day_s], $topics_per_day),
-	'TOPICS_PER_YEAR'	=> sprintf($this->user->lang[$l_topics_per_year_s], $topics_per_year),
-	'TOPICS_PER_USER'	=> sprintf($this->user->lang[$l_topics_per_user_s], $topics_per_user),
-	'TOTAL_FILES'		=> sprintf($this->user->lang[$l_total_file_s], $total_files),
-	'FILES_PER_DAY'		=> sprintf($this->user->lang[$l_files_per_day_s], $files_per_day),
-	'FILES_PER_YEAR'	=> sprintf($this->user->lang[$l_files_per_year_s], $files_per_year),
-	'FILES_PER_USER'		=> sprintf($this->user->lang[$l_files_per_user_s], $files_per_user),
-	'USERS_PER_DAY'		=> sprintf($this->user->lang[$l_users_per_day_s], $users_per_day),
-	'USERS_PER_YEAR'	=> sprintf($this->user->lang[$l_users_per_year_s], $users_per_year),
-	'START_DATE'		=> $start_date,
+		$this->template->assign_vars(array(
+			'POSTS_PER_DAY'		=> sprintf($this->user->lang[$l_posts_per_day_s], $posts_per_day),
+			'POSTS_PER_YEAR'	=> sprintf($this->user->lang[$l_posts_per_year_s], $posts_per_year),
+			'POSTS_PER_TOPIC'	=> sprintf($this->user->lang[$l_posts_per_topic_s], $posts_per_topic),
+			'POSTS_PER_USER'		=> sprintf($this->user->lang[$l_posts_per_user_s], $posts_per_user),
+			'TOPICS_PER_DAY'		=> sprintf($this->user->lang[$l_topics_per_day_s], $topics_per_day),
+			'TOPICS_PER_YEAR'	=> sprintf($this->user->lang[$l_topics_per_year_s], $topics_per_year),
+			'TOPICS_PER_USER'	=> sprintf($this->user->lang[$l_topics_per_user_s], $topics_per_user),
+			'TOTAL_FILES'		=> sprintf($this->user->lang[$l_total_file_s], $total_files),
+			'FILES_PER_DAY'		=> sprintf($this->user->lang[$l_files_per_day_s], $files_per_day),
+			'FILES_PER_YEAR'	=> sprintf($this->user->lang[$l_files_per_year_s], $files_per_year),
+			'FILES_PER_USER'		=> sprintf($this->user->lang[$l_files_per_user_s], $files_per_user),
+			'USERS_PER_DAY'		=> sprintf($this->user->lang[$l_users_per_day_s], $users_per_day),
+			'USERS_PER_YEAR'	=> sprintf($this->user->lang[$l_users_per_year_s], $users_per_year),
+			'START_DATE'		=> $start_date,
 		));
 	}
 
